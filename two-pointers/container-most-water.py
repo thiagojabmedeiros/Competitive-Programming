fd66200c3a0f5644b16df306a9ee61e9806b1e18
Thiago Medeiros <thiagojabm@gmail.com>
2026-05-13 10:19:57 -0300

    brute force solution 2 to containers

--- two-pointers/container-most-water.py
+++ two-pointers/container-most-water.py
@@ -4,17 +4,10 @@ def containerMostWater(arr):
     for l in range(len(arr)):
         containers[l] = []
         for r in range(1, len(arr)):
-            if arr[l] == 1:
-                containers[l] = [1] * (abs(l - r))
-                maxC = max(maxC, 1)
-                continue
-            if arr[l] <= arr[r]:
-                value = arr[l] * (abs(l - r))
-                containers[l].append(value)
-                maxC = max(maxC, value)
-            
+            value = min(arr[r], arr[l]) * (abs(l - r))
+            containers[l].append(value)
+            maxC = max(maxC, value)
     print(containers)
     print(maxC)
 
-
-containerMostWater([1,7,2,5,4,7,3,6])
+containerMostWater([2,2,2])
