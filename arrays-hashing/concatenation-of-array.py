6f2f1989429881237760b63889bdb332c9a23166
Thiago Medeiros <thiagojabm@gmail.com>
2026-07-20 22:02:00 -0300

    restart praticing problem sets

--- arrays-hashing/concatenation-of-array.py
+++
@@ -1,9 +0,0 @@
-def concatenation(nums):
-    ans = []
-    for i in range(2 * len(nums)):
-        if i > len(nums) - 1:
-            i -= len(nums)
-        ans.append(nums[i])
-    print(ans)
-    
-concatenation([1,2,3,4])
--- arrays-hashing/valid-anagram.py
+++
@@ -1,8 +0,0 @@
-def isAnagram(s, t):
-    s = "".join(sorted(s))
-    t = "".join(sorted(t))
-    return s == t
-
-t = "thiago"
-s = "ogaith"
-print(isAnagram(s, t))
